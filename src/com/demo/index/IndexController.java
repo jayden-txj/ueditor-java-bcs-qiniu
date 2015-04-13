a9c5455a4916df28b4840b825a0c5e23e066b9dd
uikoo9 <uikoo9@qq.com>
2015-04-14 07:34:16 +0800

    初步完成

---
+++ src/com/demo/index/IndexController.java
@@ -0,0 +1,26 @@
+package com.demo.index;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.jfinal.core.Controller;
+
+public class IndexController extends Controller {
+	public void index() {
+		setAttr("title", "uikoo9.com");
+		setAttr("devMode", true);
+		setAttr("base", getHttpPath(getRequest()));
+		
+		render("/WEB-INF/view/index.ftl");
+	}
+	
+	private String getHttpPath(HttpServletRequest request){
+		StringBuilder path = new StringBuilder();
+		
+		path.append(request.getScheme() + "://");
+		path.append(request.getServerName() + ":");
+		path.append(request.getServerPort());
+		path.append(request.getContextPath());
+		
+		return path.toString();
+	}
+}
